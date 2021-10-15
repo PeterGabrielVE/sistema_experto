2a1da6e93c7234105f9ee68322c7e888285b117e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-10-15 09:32:52 -0400

    modificacion migracion

--- database/migrations/2021_10_15_133052_create_patient_table.php
+++ database/migrations/2021_10_15_133052_create_patient_table.php
@@ -18,10 +18,10 @@ class CreatePatientTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('address');
-            $table->integer('city');
-            $table->integer('country');
+            $table->integer('id_region');
+            $table->integer('id_comuna');
             $table->string('image');
-            $table->string('comment');
+            $table->string('comment')->nullable();
             $table->date('birthdate');
             $table->char('gender', 1);
             $table->timestamps();
