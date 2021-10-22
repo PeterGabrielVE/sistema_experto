13d24b2581ac918b2838caeaad31f40df17401d3
Pedro Gabriel <gaboleal123@gmail.com>
2021-10-22 11:26:05 -0400

    cambios

---
+++ database/migrations/2021_10_22_143215_create__schedule_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_schedule', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('breakfast');
+            $table->string('lunch');
+            $table->string('dinner');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_schedule');
+    }
+}
