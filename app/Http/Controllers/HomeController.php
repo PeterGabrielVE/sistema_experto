5d85044cafeaad174eb478af05bcf881012eb52f
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2021-10-29 10:01:53 -0400

    ajustes de deshboad terminadooo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Recommendation;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $recomm = Recommendation::paginate(10);
+        $users = User::paginate(10);
+        return view('home',compact('recomm','users'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,8 +97,9 @@
             <div class="table-full-width table-responsive">
               <table class="table">
                 <tbody>
+                  @foreach($recomm as $re)
                   <tr>
-                    <td class="text-left">Sign contract for "What are conference organizers afraid of?"</td>
+                    <td class="text-left">{{ $re->description ?? null }}</td>
                     <td class="td-actions text-right">
                       <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
                         <i class="now-ui-icons ui-2_settings-90"></i>
@@ -108,37 +109,17 @@
                       </button>
                     </td>
                   </tr>
-                  <tr>
-                    <td class="text-left">Lines From Great Russian Literature? Or E-mails From My Boss?</td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                        <i class="now-ui-icons ui-2_settings-90"></i>
-                      </button>
-                      <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                        <i class="now-ui-icons ui-1_simple-remove"></i>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="text-left">Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                    </td>
-                    <td class="td-actions text-right">
-                      <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                        <i class="now-ui-icons ui-2_settings-90"></i>
-                      </button>
-                      <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                        <i class="now-ui-icons ui-1_simple-remove"></i>
-                      </button>
-                    </td>
-                  </tr>
+                  @endforeach
                 </tbody>
               </table>
+              {{ $recomm->links() }}
+
             </div>
           </div>
           <div class="card-footer ">
             <hr>
             <div class="stats">
-              <i class="now-ui-icons loader_refresh spin"></i> Updated 3 minutes ago
+              <i class="now-ui-icons loader_refresh spin"></i> Actualizado hace 3 minutos
             </div>
           </div>
         </div>
@@ -146,99 +127,26 @@
       <div class="col-md-6">
         <div class="card">
           <div class="card-header">
-            <h5 class="card-category">All Persons List</h5>
-            <h4 class="card-title"> Employees Stats</h4>
+            <h5 class="card-category">Lista de todos los doctores</h5>
+            <h4 class="card-title">Doctores</h4>
           </div>
           <div class="card-body">
             <div class="table-responsive">
               <table class="table">
                 <thead class=" text-primary">
-                  <th>
-                    Name
-                  </th>
-                  <th>
-                    Country
-                  </th>
-                  <th>
-                    City
-                  </th>
-                  <th class="text-right">
-                    Salary
-                  </th>
+                  <th>Nombres</th>
+                  <th>Correo</th>
                 </thead>
                 <tbody>
+                  @foreach($users as $u)
                   <tr>
-                    <td>
-                      Dakota Rice
-                    </td>
-                    <td>
-                      Niger
-                    </td>
-                    <td>
-                      Oud-Turnhout
-                    </td>
-                    <td class="text-right">
-                      $36,738
-                    </td>
+                    <td>{{ $u->name ?? null }}</td>
+                    <td>{{ $u->email ?? null }}</td>
                   </tr>
-                  <tr>
-                    <td>
-                      Minerva Hooper
-                    </td>
-                    <td>
-                      Curaçao
-                    </td>
-                    <td>
-                      Sinaai-Waas
-                    </td>
-                    <td class="text-right">
-                      $23,789
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      Sage Rodriguez
-                    </td>
-                    <td>
-                      Netherlands
-                    </td>
-                    <td>
-                      Baileux
-                    </td>
-                    <td class="text-right">
-                      $56,142
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      Doris Greene
-                    </td>
-                    <td>
-                      Malawi
-                    </td>
-                    <td>
-                      Feldkirchen in Kärnten
-                    </td>
-                    <td class="text-right">
-                      $63,542
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      Mason Porter
-                    </td>
-                    <td>
-                      Chile
-                    </td>
-                    <td>
-                      Gloucester
-                    </td>
-                    <td class="text-right">
-                      $78,615
-                    </td>
-                  </tr>
+                  @endforeach
                 </tbody>
               </table>
+              {{ $users->links() }}
             </div>
           </div>
         </div>
