031447154d8aa2a41ef4dd56097ff7978f9cf49e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-02-17 22:26:23 -0400

    arreglo

--- resources/views/diagnoses/result.blade.php
+++ resources/views/diagnoses/result.blade.php
@@ -452,19 +452,7 @@
                 "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
             },
             "order": [[ 0, "desc" ]],
-            "initComplete": function () {
-                this.api().columns().every( function () {
-                    var that = this;
-
-                    $( 'input', this.footer() ).on( 'keyup change', function () {
-                        if ( that.search() !== this.value ) {
-                            that
-                                .search( this.value )
-                                .draw();
-                            }
-                    });
-                })
-            }
+           
     });
 
 });
